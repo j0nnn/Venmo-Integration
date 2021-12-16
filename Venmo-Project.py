8415f620ade5e4a945511dd72086086e6d38b48b
j0nnn <jsun63@berkeley.edu>
2021-12-16 03:30:13 -0800

    get recent transactions that display actor and target

---
+++ Venmo-Project.py
@@ -0,0 +1,14 @@
+from venmo_api import Client
+
+# Get your access token. You will need to complete the 2FA process
+# Please store it somewhere safe and use it next time
+# Never commit your credentials or token to a git repository
+                                  
+# Initialize api client using an access-token
+client = Client(access_token="806a651ec53d2b8b8e28090bd1114aee6920a0cd748e4ee1b8bfd0258582f529")
+
+bruh = client.user.get_user_transactions(user_id = "2912360473821184765")
+for lmao in bruh:
+    print(lmao.actor.display_name, " -> ", lmao.target.display_name)
+#2912360473821184765
+
