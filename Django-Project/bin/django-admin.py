e3acd4c4aae1bffa82529488e41e2feab4f96030
Leo Huang <klhftco@berkeley.edu>
2021-12-29 01:59:27 -0800

    clean

--- Django-Project/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Totoro/Downloads/Fall 2021/TASA/API/Venmo-Integration/Django-Project/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
